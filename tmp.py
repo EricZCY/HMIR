07cb29d6a6fc7caf27d725e79161d59a95ad66c0
EricZCY <62788624+EricZCY@users.noreply.github.com>
2020-06-10 15:26:05 +0800

    Add files via upload

---
+++ tmp.py
@@ -0,0 +1,20 @@
+def hcisexit():
+    cuntinuehcis = 0
+    print()
+def hprint(text):
+    print(text)
+def calc(item):
+    print(item)
+def run(filename):
+    runfile(filename)
+def port(thing):
+    if thing == "drawing":
+        import turtle as drawing
+        print("Drawing ported!")
+    elif thing == "time":
+        import time
+        print("Time ported!")
+    elif thing == "random":
+        import random
+        print("Random ported!")
+hcisexit()
